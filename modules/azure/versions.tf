051c0297341c9455e3b4f7fc00a1a2aa1f606915
mallory.brickerd <mallory.brickerd@eraneos.com>
2025-03-05 16:48:01 +0100

    fix: applied Azure module stash

---
+++ modules/azure/versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0, < 4.0.0"
+    }
+  }
+}
+
+# tflint-ignore: terraform_unused_declarations
+variable "module_version" {
+  type        = string
+  default     = "0.0.1"
+  description = "Current version of the Azure Terraform modules."
+}
